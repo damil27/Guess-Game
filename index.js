e14faad59f2bb515e5a51dc5dfbecb5357945460
FADODUN DAMILARE <>
2022-10-22 02:16:17 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,26 @@
+
+
+
+let currentPoint = 0;
+let attemptLeft = 10;
+let max = 2;
+let min  = 1;
+let i = 0
+let guess = prompt(`enter your guess number btw ${min} and ${max}`)
+let secretNum = Math.trunc(Math.random()* max) + min;
+
+  
+  if(secretNum == guess){
+    currentPoint += 1
+    console.log(`corrrent guess, you have ${currentPoint}`)
+    max++
+   
+  }else{
+    console.log(`try again, you have ${currentPoint}`);
+    guess = prompt(`enter your guess number btw ${min} and ${max}`)
+  }
+
+     
+  
+
+
